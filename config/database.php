92fe3c464805a73ba6470087712ec73a7d604b98
Marcelo Emanuel Quiroga <quirogatdf@gmail.com>
2026-01-30 15:48:55 -0300

    fix(database): optimize PDO options to prevent connection timeouts on Raylway

--- config/database.php
+++ config/database.php
@@ -59,6 +59,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::ATTR_TIMEOUT => 5,
+
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
